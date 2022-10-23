14ab059e46b4ec97e1d16a2f2ddb92b25c71a10f
Web-Mustafiz <mustafiz.kst2020@gmail.com>
2022-10-23 14:42:39 +0600

    Panda News Portal site project setup

---
+++ src/Routes/Routes/Routes.js
@@ -0,0 +1,10 @@
+import { createBrowserRouter, Outlet } from "react-router-dom";
+import Main from "../../layouts/Main";
+
+export const routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children: [],
+  },
+]);
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDB8_CkaIlIssjPMt6mzrByxfbbbzTEH1g",
+  authDomain: "panda-news-da1fd.firebaseapp.com",
+  projectId: "panda-news-da1fd",
+  storageBucket: "panda-news-da1fd.appspot.com",
+  messagingSenderId: "732638442912",
+  appId: "1:732638442912:web:34b8d902f74058560820c5",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
